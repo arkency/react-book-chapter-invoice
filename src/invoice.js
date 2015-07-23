a84c7251b8d7bb95903b707506124b8921f1a149
Piotr Macuk <piotr@macuk.pl>
2015-07-23 12:15:36 +0200

    First component scaffold

---
+++ src/invoice.js
@@ -0,0 +1,10 @@
+class InvoiceLineItems extends React.Component {
+  render() {
+    return(<span>Invoice Line Items Component</span>);
+  }
+}
+
+React.render(
+  <InvoiceLineItems />,
+  document.getElementById("invoice")
+);
