ff78a4e0bca0b18487f315f69d4aabd4481534c8
Piotr Macuk <piotr@macuk.pl>
2015-07-23 12:26:35 +0200

    LineItem elements in <table>

--- src/invoice.js
+++ src/invoice.js
@@ -1,16 +1,31 @@
 class LineItem extends React.Component {
   render() {
-    return(<div>Line Item {this.props.number}</div>);
+    return(
+      <tr>
+        <td>{this.props.number}.</td>
+        <td><input name="title" /></td>
+        <td><input name="pricePerUnit" /></td>
+        <td><input name="amount" /></td>
+        <td>...</td>
+      </tr>
+    );
   }
 }
 
 class InvoiceLineItems extends React.Component {
   render() {
     return(
-      <div>
+      <table>
+        <tr>
+          <th>No</th>
+          <th>Title</th>
+          <th>Price per unit</th>
+          <th>Amount</th>
+          <th>Total price</th>
+        </tr>
         <LineItem number="1" />
         <LineItem number="2" />
-      </div>
+      </table>
     );
   }
 }
