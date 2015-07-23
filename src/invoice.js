3b4335eb649e8c8bcff3c9e0b6088e474ff9f9d9
Piotr Macuk <piotr@macuk.pl>
2015-07-23 12:53:31 +0200

    Better UI with Bootstrap

--- src/invoice.js
+++ src/invoice.js
@@ -2,11 +2,12 @@ class LineItem extends React.Component {
   render() {
     return(
       <tr>
-        <td>{this.props.number}.</td>
-        <td><input name="title" /></td>
-        <td><input name="pricePerUnit" /></td>
-        <td><input name="amount" /></td>
-        <td>...</td>
+        <td width="1%">{this.props.number}.</td>
+        <td width="50%"><input name="title" className="form-control"/></td>
+        <td width="15%"><input name="pricePerUnit" className="form-control" /></td>
+        <td width="15%"><input name="amount" className="form-control" /></td>
+        <td width="15%">...</td>
+        <td width="4%">...</td>
       </tr>
     );
   }
@@ -15,16 +16,18 @@ class LineItem extends React.Component {
 class InvoiceLineItems extends React.Component {
   render() {
     return(
-      <table>
+      <table className="table table-bordered table-hover">
         <tr>
-          <th>No</th>
+          <th>Nr</th>
           <th>Title</th>
           <th>Price per unit</th>
           <th>Amount</th>
           <th>Total price</th>
+          <th>Action</th>
         </tr>
         <LineItem number="1" />
         <LineItem number="2" />
+        <LineItem number="3" />
       </table>
     );
   }
